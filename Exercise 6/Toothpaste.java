1960f93ffe36299ed767ffa7229e76a49c18381c
Carlos III Regodon <119915173+Zeddqt@users.noreply.github.com>
2023-02-10 11:41:57 +0800

    Create Toothpaste.java

---
+++ Exercise 6/Toothpaste.java
@@ -0,0 +1,9 @@
+public class Toothpaste extends Groceryitem {
+    
+    
+    public void showitemname(){
+        System.out.println(this.name + " is a Toothpaste" );
+        
+        
+    }
+}
